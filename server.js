c1e8a40239d2aa68b76c416ffed90148f51282d3
MYTSALUDIPS <120595142+MYTSALUDIPS@users.noreply.github.com>
2025-11-15 18:25:50 -0500

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const fs = require("fs");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import mysql from "mysql2";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+import dotenv from "dotenv";
 
-// APP
+dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// PUERTO RENDER
 const PORT = process.env.PORT || 4000;
 
-// MySQL
-const mysql = require("mysql2");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const db = mysql.createPool({
+// Conexion DB
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
@@ -25,22 +28,21 @@ const db = mysql.createPool({
   connectionLimit: 10,
 });
 
-// Test de conexión
 db.getConnection((err, conn) => {
   if (err) {
-    console.error("❌ Error conectando MySQL:", err);
+    console.error("❌ Error conectando a MySQL:", err);
   } else {
-    console.log("✅ MySQL conectado");
+    console.log("✅ Conectado a MySQL");
     conn.release();
   }
 });
 
 // RUTAS
-const maestraRoutes = require("./routes/maestra");
-const kardexRoutes = require("./routes/kardex");
-const usuariosRoutes = require("./routes/usuarios");
-const pedidosRoutes = require("./routes/pedidos");
-const despachoRoutes = require("./routes/despacho");
+import maestraRoutes from "./routes/maestra.js";
+import kardexRoutes from "./routes/kardex.js";
+import usuariosRoutes from "./routes/usuarios.js";
+import pedidosRoutes from "./routes/pedidos.js";
+import despachoRoutes from "./routes/despacho.js";
 
 app.use("/api/maestra", maestraRoutes);
 app.use("/api/kardex", kardexRoutes);
@@ -48,18 +50,19 @@ app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/pedidos", pedidosRoutes);
 app.use("/api/despacho", despachoRoutes);
 
-// FRONTEND
+// STATIC FILES
 const publicPath = path.join(__dirname, "public");
 app.use(express.static(publicPath));
 
-app.get("/", (_, res) => res.sendFile(path.join(publicPath, "login.html")));
+app.use("/formatos", express.static(path.join(publicPath, "formatos")));
 
-// PDFs
 const pdfDir = path.join(process.cwd(), "despachos_pdfs");
 if (!fs.existsSync(pdfDir)) fs.mkdirSync(pdfDir, { recursive: true });
 app.use("/despachos_pdfs", express.static(pdfDir));
 
-// INICIAR SERVIDOR
+app.get("/", (_, res) => res.sendFile(path.join(publicPath, "login.html")));
+
+// SERVER LISTEN
 app.listen(PORT, () => {
   console.log("🚀 Servidor corriendo en puerto", PORT);
 });
