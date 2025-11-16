466a94e05a0a4eb89136dda8b28302b2207374ae
MYTSALUDIPS <120595142+MYTSALUDIPS@users.noreply.github.com>
2025-11-15 20:18:15 -0500

    server.js

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ import { fileURLToPath } from "url";
 import fs from "fs";
 import dotenv from "dotenv";
 
-<<<<<<< HEAD
 // Cargar variables de entorno (.env)
-=======
->>>>>>> 7ae9b534336f25828094969cfb755c515f5d7382
 dotenv.config();
 
 // App express
@@ -18,24 +15,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-// Puerto Clever Cloud usa process.env.PORT
-=======
 // Puerto Render usa process.env.PORT
->>>>>>> 7ae9b534336f25828094969cfb755c515f5d7382
 const PORT = process.env.PORT || 4000;
 
 // Resolver rutas
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// ===============================
-<<<<<<< HEAD
-// 🔥 CONEXIÓN MYSQL (Clever Cloud / Local)
-=======
 // 🔥 CONEXIÓN MYSQL (Render / Local)
->>>>>>> 7ae9b534336f25828094969cfb755c515f5d7382
-// ===============================
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -51,18 +38,12 @@ db.getConnection((err, conn) => {
   if (err) {
     console.error("❌ Error conectando a MySQL:", err);
   } else {
-<<<<<<< HEAD
-    console.log("✅ Conectado a MySQL (Clever Cloud)");
-=======
-    console.log("✅ Conectado a MySQL (Render)");
->>>>>>> 7ae9b534336f25828094969cfb755c515f5d7382
+    console.log("✅ Conectado a MySQL");
     conn.release();
   }
 });
 
-// ===============================
 // 🔥 RUTAS (CORRECTAS)
-// ===============================
 import maestraRoutes from "./routes/maestra.js";
 import kardexRoutes from "./routes/kardex.js";
 import usuariosRoutes from "./routes/usuarios.js";
@@ -75,9 +56,7 @@ app.use("/api/usuarios", usuariosRoutes);
 app.use("/api/pedidos", pedidosRoutes);
 app.use("/api/despacho", despachoRoutes);
 
-// ===============================
 // 🔥 FRONTEND (carpeta public)
-// ===============================
 const publicPath = path.join(__dirname, "public");
 app.use(express.static(publicPath));
 
@@ -87,9 +66,7 @@ const pdfDir = path.join(process.cwd(), "despachos_pdfs");
 if (!fs.existsSync(pdfDir)) fs.mkdirSync(pdfDir, { recursive: true });
 app.use("/despachos_pdfs", express.static(pdfDir));
 
-// ===============================
 // 🔥 RUTAS HTML
-// ===============================
 const sendHtml = (file, res) => {
   res.sendFile(path.join(publicPath, file));
 };
@@ -103,9 +80,8 @@ app.get("/pedidos.html", (_, res) => sendHtml("pedidos.html", res));
 app.get("/usuarios.html", (_, res) => sendHtml("usuarios.html", res));
 app.get("/despacho.html", (_, res) => sendHtml("despacho.html", res));
 
-// ===============================
 // 🔥 INICIAR SERVER
-// ===============================
 app.listen(PORT, () => {
   console.log("🚀 Servidor corriendo en puerto", PORT);
 });
+
